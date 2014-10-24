60e615bfe27a721b60b1657bfca56c0faabc5df8
Sergio <rurounisergii@hotmail.com>
2014-10-24 21:00:23 +0100

    adding ideas

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -4,9 +4,6 @@ public class FractionCalculator{
 		FractionCalc.launch();
 	}
 	public void launch(){
-		String operator = "";
-		Fraction initialValue = new Fraction(0,1);
-
 	    System.out.println("Hello my name is Sergio Lamela");
 		System.out.println("Student Number: 12944473");
 		System.out.println("Welcome to the amazing Fraction Calculator");
@@ -14,17 +11,29 @@ public class FractionCalculator{
 		System.out.println("Please enter what you would like to calculate.");
 		System.out.println("To end, insert Q");
 
+		//User String input and initial fraction value will be put into evaluate method
+		//each iteration. The evaluate method will print the final result of
+		//evaluating the user's line of input. The initialValue fraction will become
+		//the final output of the previous iteration.
+
+		Fraction initialValue = new Fraction(0,1); //this is equivalent to 0
+
 		do{
 			System.out.println("Enter line of input: ");
-
-
-
+			String userInput = System.console().readLine();
+			evaluate(initialValue, userInput);
 		}while (System.console().readLine() != "q" || System.console().readLine() != "Q");
 
 	}
 
 	public void evaluate(Fraction fraction, String inputString){
-		Fraction startingValue = fraction;
+		Fraction resultValue = fraction;
 		String textInput = inputString;
+		String operator = "";
+		int position = 0;
+		int position2 = 0;
+		while (position < textInput.length()){
+
+		}
 	}
 }
