f6cb37fda1064d81021fa1e83dcf9446e6bc4339
Sergio <rurounisergii@hotmail.com>
2014-10-24 19:42:48 +0100

    adding some stuff to fractionCalculator

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -1,5 +1,9 @@
 public class FractionCalculator{
 	public static void main(String[] args){
+		FractionCalculator FractionCalc = new FractionCalculator();
+		FractionCalc.launch();
 	}
+	public void launch(){
 
+	}
 }
